72c4df23993e1f9fd4f691835f9f2dcb2435bf1c
codesaveslives <sirelexzy@gmail.com>
2025-08-15 21:29:53 +0100

    Update voting eligibility checker with input validation

--- JS Mini Project Two/index.js
+++ JS Mini Project Two/index.js
@@ -1,4 +1,5 @@
 /*
+
 MY INITIAL SOLUTION
 
 const votingAge = 18
@@ -9,6 +10,7 @@ let age = Number(prompt("Enter Your Age: "))
 if (age >= 18) {
     console.log(`Hello ${name} you are eligible to vote.`)
 }
+
 else {
     console.log(`Hello ${name} you are not eligible to vote.`)
 }
@@ -16,12 +18,19 @@ else {
 
 const votingAge = 18;
 
-const name = prompt("Enter Your Name: ");
-const age = Number(prompt("Enter Your Age: "))
+const name = prompt("Enter your name: ");
+const age = Number(prompt("Enter your age: "))
 
-if (age >= 18) {
+if (isNaN(age) || age < 0 ) {
+    console.log(`Please enter a number.`)
+}
+
+else if (age >= votingAge) {
     console.log(`Hello ${name}, You are eligible to vote.`)
 }
-else {
-    console.log(`Hello ${name}, You are not eligible to vote.`)
+
+else  {
+    console.log(`Hello ${name}, You are not eligible to vote`)
 }
+
+// Else doesn't take syntax only if and else if
