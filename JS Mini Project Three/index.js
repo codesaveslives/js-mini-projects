2610c00314793d202fc73f850f43331368572c93
codesaveslives <sirelexzy@gmail.com>
2025-08-16 19:02:55 +0100

    Add JS Mini Project Three

---
+++ JS Mini Project Three/index.js
@@ -0,0 +1,154 @@
+/*
+
+// Arithmetic Playground Challenge 2
+
+
+let a = 12;
+let b = 5;
+
+let sum = a + b;
+console.log(sum);
+
+let difference = a - b;
+console.log(difference);
+
+let product = a * b;
+console.log(product);
+
+let quotient = a / b;
+console.log(quotient)
+
+let remainder = a % b;
+console.log(remainder);
+
+let exponent = a ** b;
+console.log(exponent);
+
+console.log(a++);
+console.log(a);
+
+console.log(++a);
+console.log(a);
+
+console.log(b--);
+console.log(b);
+
+console.log(--b);
+console.log(b);
+
+*/
+
+/*
+// COMPOUND ASSIGNMENT CHALLENGE 4.
+
+let x = 20
+
+x += 1;
+console.log(x);
+
+x -= 1;
+console.log(x);
+
+x *= 2;
+console.log(x);
+
+x /= 2;
+console.log(x);
+
+x %= 3;
+console.log(x);
+*/
+
+/*
+// TRUTH OR FALSY CHECK CHALLENGE 5
+
+let x = [0, "", null, undefined, "Hello", 42];
+
+if (x[0]) {
+    console.log(x[0], "is true")
+}
+else {
+    console.log(x[0], "is false")
+}
+
+if (x[1]) {
+    console.log(x[1], "is true")
+}
+else {
+    console.log(x[1], "is false")
+}
+
+if (x[2]) {
+    console.log(x[2], "is true")
+}
+else {
+    console.log(x[2], "is false")
+}
+
+if (x[3]) {
+    console.log(x[3], "is true")
+}
+else {
+    console.log(x[3], "is false")
+}
+
+if (x[4]) {
+    console.log(x[4], "is true")
+}
+else {
+    console.log(x[4], "is false")
+}
+
+if (x[5]) {
+    console.log(x[5], "is true")
+}
+else {
+    console.log(x[5], "is false")
+}
+
+// for loop practice.
+
+for (let i = 0; i < x.length; i++) {
+    if (x[i]){
+        console.log(x[i], "is truthy")
+    }
+    else {
+        console.log(x[i], "is falsy")
+    }
+}
+
+*/
+
+/*
+
+// CHALLENGE 6: TYPE CONVERSION.
+
+let hundred = "100";
+hundred = Number(hundred);
+
+let sum = hundred + 50;
+sum = String(sum);
+
+console.log(sum);
+
+console.log(typeof(sum));
+
+*/
+
+// CHALLENGE 7: LOGIN SIMULATION
+hasUsername = true;
+hasPassword = false;
+
+if (hasUsername && hasPassword) {
+    console.log("Login Successful")
+}
+
+/*
+else if (hasUsername || hasPassword){
+    console.log("Login Failed")
+}
+*/ // else if is doing same work with else so it is redundant.
+
+else {
+    console.log("Login Failed")
+}
