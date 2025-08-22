5c1404527f51d909dc70c5119b6667b216ac95e1
codesaveslives <sirelexzy@gmail.com>
2025-08-22 19:22:06 +0100

    Add JS Password Checker

---
+++ JS Password Checker/index.js
@@ -0,0 +1,32 @@
+/*
+
+// Password Checker(Advanced)
+// Goal: create a function that stores a password and returns a function to check it.
+
+const checkPassword = createPasswordChecker("secret123")
+
+console.log(checkPassword("test")); // false
+console.log(checkPassword("secret123")); // true
+
+// your task: write createPasswordChecker
+
+function createPasswordChecker(password) {
+    // return a function that takes input and checks against password
+}
+
+*/
+
+function createPasswordChecker() {
+    return function (password) {
+        if (password == "secret123") {
+            console.log("true");
+        }
+        else{
+            console.log(false);
+        }
+        return password
+    }
+}
+
+const checkPassword = createPasswordChecker()
+console.log(checkPassword("test"))
