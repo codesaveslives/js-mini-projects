8b19bfff6e4efe55c5aa9948fd68320b47abeeb5
codesaveslives <sirelexzy@gmail.com>
2025-08-24 16:54:43 +0100

    Add While loop Challenge

---
+++ While loop Challenge/challenge.js
@@ -0,0 +1,14 @@
+// Beginner Challenge
+
+// 1
+// count from 1 to 10.
+// use a while loop to print through numbers 1 through 10.
+
+// solution
+
+let num = 0;
+
+while(num < 10) {
+    num++;
+    console.log(num);
+}
