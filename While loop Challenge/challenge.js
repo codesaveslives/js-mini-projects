64a9cef12d142b52d010cf60f2265c95321f14e1
codesaveslives <sirelexzy@gmail.com>
2025-08-25 13:20:12 +0100

    Uploaded  Challenge 2

--- While loop Challenge/challenge.js
+++ While loop Challenge/challenge.js
@@ -1,5 +1,6 @@
 // Beginner Challenge
 
+/*
 // 1
 // count from 1 to 10.
 // use a while loop to print through numbers 1 through 10.
@@ -12,3 +13,30 @@ while(num < 10) {
     num++;
     console.log(num);
 }
+*/
+
+/*
+
+// 2
+// Print all even numbers from 2 to 20 using while loops.
+
+// Using Modulus
+let numbers = 0;
+
+while (numbers < 20) {
+    numbers++
+    if (numbers % 2 === 0) {
+        console.log(`${numbers}, is an even number.`);
+    }
+    
+}
+
+// Using addition assignment operator
+let number = 2 
+
+while (number < 20) {
+    console.log(`${number}, is an even number`);
+    number+=2
+}
+
+*/
